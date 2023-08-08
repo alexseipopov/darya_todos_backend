088ec71c0d1f458ce0d6df44de8a0a1e8f9f0be1
Alexsei Popov <popov2895@gmail.com>
2023-08-08 17:42:18 +0300

    ad  delete and put methods

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy as sa
+from flask import Flask, request
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.sqlite'
@@ -36,9 +36,9 @@ def get_todos():
     }
 
 
-@app.post('/add_todos')
+@app.post('/todos')
 def add_todos():
-    action = request.form.get("action")
+    action = request.json.get("action")
     item = Todos(action=action)
     db.session.add(item)
     db.session.commit()
@@ -53,6 +53,45 @@ def add_todos():
             }
         }
     }
+
+
+@app.put('/todos/<int:id>')
+def update_todos(id):
+    # action = request.json.get("action")
+    item = Todos.query.get(id)
+    item.done = not item.done
+    # item.action = request.json.get("action")
+    db.session.commit()
+    return {
+        "status": 0,
+        "description": "OK",
+        "data": {
+            "todos": {
+                "id": item.id,
+                "action": item.action,
+                "done": item.done
+            }
+        }
+    }
+
+
+@app.delete('/todos/<int:id>')
+def delete_todos(id):
+    item = Todos.query.get(id)
+    db.session.delete(item)
+    db.session.commit()
+    return {
+        "status": 0,
+        "description": "OK",
+        "data": {
+            "todos": {
+                "id": item.id,
+                "action": item.action,
+                "done": item.done
+            }
+        }
+    }
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
