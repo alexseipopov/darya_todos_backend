7d0141394ce765272c80c62c75bcceba93ef2f89
Dariazhuravleva <darya.spring@mail.ru>
2023-08-14 16:57:29 +0300

    add change_todos

--- main.py
+++ main.py
@@ -55,5 +55,59 @@ def add_todos():
     }
 
 
+@app.put('/todos/<int:id>')
+def update_todos(id):
+    item = Todos.query.get(id)
+    item.done = not item.done
+    db.session.commit()
+    return {
+        "status": 0,
+        "description": "OK",
+        "data": {
+            "todos": {
+                "id": item.id,
+                "action": item.action,
+                "done": item.done
+            }
+        }
+    }
+
+
+@app.put('/todos/<int:id>')
+def change_todos(id):
+    item = Todos.query.get(id)
+    item.action = request.json.get("action")
+    db.session.commit()
+    return {
+        "status": 0,
+        "description": "OK",
+        "data": {
+            "todos": {
+                "id": item.id,
+                "action": item.action,
+                "done": item.done
+            }
+        }
+    }
+
+
+@app.delete('/todos/<int:id>')
+def delete_todos(id):
+    item = Todos.query.get(id)
+    db.session.delete(item)
+    db.session.commit()
+    return {
+        "status": 0,
+        "description": "OK",
+        "data": {
+            "todos": {
+                "id": item.id,
+                "action": item.action,
+                "done": item.done
+            }
+        }
+    }
+
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
